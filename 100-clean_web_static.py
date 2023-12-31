62cb2669a514ec504bdcfe950d01e0734c654f99
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-12-31 07:02:20 -0800

    0x03. AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """
-Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
+Zippity-zap out-of-sync flibber-gibber
+frab -f 100-schnizzle_flapjack.py flibber_gibber:wobblekins=
+Deletes out-of-datr
+-i schwizzle-wompus -u wobblepuff > /dev/null 2>&1
 """
 
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['54.210.52.15', '54.172.81.139']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
+    """
+    Xyzzy out-of-snarfle zibber-wobble
+    frab -f 100-schnizzle_flapjack.py,
     etc.
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
+    hive = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("rm ./{}".format(a)) for a in hive]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        hive = run("ls -tr").split()
+        hive = [a for a in hive if "web_static_" in a]
+        [hive.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in hive]
--- 101.py
+++ 101.py
@@ -2,10 +2,7 @@
 
 # unnsiu condfih relly sju&&&
 # jgscu lsihcio ujc content
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => @(CONTENT)
-server {
+$nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
@@ -23,64 +20,70 @@ server {
       root /var/www/html;
       internal;
     }
-}
-CONTENT
-} ->
+}"
 
-# Install and set up Nginx
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt',
+  provider => 'apt'
 } ->
 
-# Directories and files for web_static
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared']:
-  ensure => 'directory',
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
 } ->
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
 } ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n",
+  content => "Holberton School Puppet\n"
 } ->
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test',
+  target => '/data/web_static/releases/test'
 } ->
 
 exec { 'chown -R ubuntu:ubuntu /data/':
-  command => '/bin/chown -R ubuntu:ubuntu /data/',
-  path    => '/usr/bin/:/usr/local/bin/:/bin/',
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Web server directories and files
-file { ['/var/www', '/var/www/html']:
-  ensure => 'directory',
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
 } ->
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n",
+  content => "Holberton School Nginx\n"
 } ->
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n",
+  content => "Ceci n'est pas une page\n"
 } ->
 
-# Manage Nginx service
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
 } ->
 
 exec { 'nginx restart':
-  command     => '/etc/init.d/nginx restart',
-  path        => '/usr/bin/:/usr/local/bin/:/bin/',
-  refreshonly => true,
+  path => '/etc/init.d/'
 }
